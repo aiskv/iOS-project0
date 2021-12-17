a03f29572629102b3b589d99b0a5a6376be942b6
AndreySkvortsov <skvortsov.andrei@phystech.edu>
2021-12-17 21:25:46 +0300

    Initial commit

---
+++ Chetverochka/MyCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MyCollectionViewCell.swift
+//  Chetverochka
+//
+//  Created by Andrey Skvortsov on 13.12.2021.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    static let identifier = "MyCollectionViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    public func configure(with image: UIImage){
+        imageView.image = image
+    }
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "MyCollectionViewCell", bundle: nil)
+    }
+}
